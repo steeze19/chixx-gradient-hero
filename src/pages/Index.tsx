e540035a60c38d9ec959d516dfc90d9c9eb81385
steeze19 <steeze266@gmail.com>
2026-01-14 02:02:13 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { MessageCircle } from "lucide-react";
 
 const Index = () => {
-  const whatsappLink = "https://chat.whatsapp.com/GxM1rxUpGF3FLCzvq9dYkt";
+  const whatsappLink = "https://chat.whatsapp.com/FrXNsk7zGflHjQ2ko82NFQ";
 
   const handleJoinWhatsApp = () => {
     window.open(whatsappLink, "_blank", "noopener,noreferrer");
